8daeb2542a27bedd30c7fe9b7dc3be0c43e345f4
umar793 <125833832+Mbilalshafqat@users.noreply.github.com>
2023-11-07 19:39:40 +0500

    solve small bugs

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 
 app.use(express.json());
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use("/", require("./userrouter"));
 
 app.listen(process.env.PORT, () => {
